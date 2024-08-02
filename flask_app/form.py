fa7dbc0f0e71f2c3641a1ee75105cc1af905b69e
trinity <ryan.cu1200@gmail.com>
2024-08-02 17:02:35 +0530

    fetching credentials from signup page

--- flask_app/form.py
+++ flask_app/form.py
@@ -5,7 +5,14 @@ from wtforms.validators import input_required, length, Email, DataRequired
 
 
 class UserLogin(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("submit")
+
+
+class SignUp(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = StringField("Password", validators=[DataRequired()])
-
+    confirm_password = StringField("Confirm Password", validators=[DataRequired()])
     submit = SubmitField("submit")
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template
-from flask_app.form import UserLogin
+from flask import render_template, request
+from flask_app.form import UserLogin, SignUp
 
 from main import app
 
@@ -13,3 +13,18 @@ def hello_world():
 def login():
     user_login = UserLogin()
     return render_template("/login.html", login=user_login)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    user_signup = SignUp()
+    if request.method == "POST" or user_signup.validate():
+        name = user_signup.name.data
+        email = user_signup.email.data
+        password = user_signup.password.data
+        confirm_password = user_signup.confirm_password.data
+
+        return f"name: {name}, email: {email}, password: {password}, confirm_password: {confirm_password}"
+    else:
+        print("not in")
+        return render_template("/signup.html", signup=user_signup)
