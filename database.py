06513e8881c588837f3736cf6e60ff3408fd82a3
trinity <ryan.cu1200@gmail.com>
2024-08-03 23:22:15 +0530

    added database file and make User and Todlist Class

---
+++ database.py
@@ -0,0 +1,21 @@
+
+from main import db, app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    db_name = db.Column(db.String, unique=True, nullable=False)
+    db_email = db.Column(db.String)
+    db_password = db.Column(db.String)
+    db_confirm_password = db.Column(db.String)
+
+
+class TodoList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    db_team_member = db.Column(db.String)
+    db_task = db.Column(db.String)
+    db_priority = db.Column(db.String)
+
+
+with app.app_context():
+    db.create_all()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_keyskjbfjksbfj_here'  # Set your secret key here
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
+db = SQLAlchemy(app)
+
 
 bootstrap = Bootstrap(app)
 
@@ -10,6 +16,7 @@ bootstrap = Bootstrap(app)
 
 
 from flask_app.routes import * # noqa
+from database import * # noqa
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
