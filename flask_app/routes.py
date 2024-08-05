c5c2ecfc3b0d9665a63ea39005f13ed5423fa834
trinity <ryan.cu1200@gmail.com>
2024-08-05 16:53:19 +0530

    Connected to the database

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, request
 from flask_app.form import UserLogin, SignUp
+from database import User
 
-from main import app
+from main import app, db
 
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return "<h1>Welcome</h1>"
 
 
 @app.route('/login')
@@ -14,6 +15,14 @@ def login():
     user_login = UserLogin()
     return render_template("/login.html", login=user_login)
 
+
+@app.route("/todo", methods=["GET", "POST"])
+def todoList():
+    if request.method == "POST":
+        topic = request.form["text"]
+        options = request.form["options"]
+        print(options)
+    return render_template("/index.html")
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -23,6 +32,15 @@ def signup():
         email = user_signup.email.data
         password = user_signup.password.data
         confirm_password = user_signup.confirm_password.data
+
+        if password == confirm_password:
+            add_users = User(db_name=name,
+                             db_email=email,
+                             db_password=password,
+                             db_confirm_password=password
+                             )
+            db.session.add(add_users)
+            db.session.commit()
 
         return f"name: {name}, email: {email}, password: {password}, confirm_password: {confirm_password}"
     else:
