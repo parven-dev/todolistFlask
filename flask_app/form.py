92c870d07788979330b55c24c9addb24592cf213
trinity <ryan.cu1200@gmail.com>
2024-08-08 17:00:54 +0530

    add login system

--- flask_app/form.py
+++ flask_app/form.py
@@ -1,22 +1,35 @@
 from flask_wtf import FlaskForm
 import email_validator
 
-
 from wtforms import (StringField, TextAreaField, IntegerField, SubmitField)
-from wtforms.validators import input_required, length, Email, DataRequired
-
+from wtforms.fields.simple import PasswordField
+from wtforms.validators import input_required, length, Email, DataRequired, InputRequired, Length, Regexp, EqualTo
 
 
 class UserLogin(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email")
     password = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("submit")
 
 
 class SignUp(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = StringField("Password", validators=[DataRequired()])
-    confirm_password = StringField("Confirm Password", validators=[DataRequired()])
+    username = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="Please provide a valid name"),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have only letters, " "numbers, dots or underscores",
+            ),
+        ]
+    )
+    email = StringField(
+        validators=[DataRequired(), Email(), length(1, 64)])
+    password = PasswordField(
+        validators=[DataRequired(),
+                    length(8, 72)])
+    confirm_password = PasswordField(
+        validators=[DataRequired(), Length(8, 72),
+                    EqualTo("password", message="Passwords must match !")])
     submit = SubmitField("submit")
-
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,8 +1,22 @@
 from flask import render_template, request
+from flask_login import login_required, logout_user, login_user, LoginManager
+
 from flask_app.form import UserLogin, SignUp
 from database import User, TodoList
 
 from main import app, db
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/")
@@ -10,10 +24,27 @@ def hello_world():
     return "<h1>Welcome</h1>"
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     user_login = UserLogin()
+    if user_login.validate() or request.method == "POST":
+        email = user_login.email.data
+        password = user_login.password.data
+        user = User.query.filter_by(db_email=email).first()
+
+        if user and user.db_password == password:
+            login_user(user)
+            return "you login successfully"
+        else:
+            return "Fuck off"
+
     return render_template("/login.html", login=user_login)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    return "<h1>user logout successfully </h1>"
 
 
 @app.route("/todo", methods=["GET", "POST"])
@@ -33,11 +64,12 @@ def todoList():
         return f"topics: {topic} options: {options}"
     return render_template("/index.html")
 
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     user_signup = SignUp()
     if request.method == "POST" or user_signup.validate():
-        name = user_signup.name.data
+        name = user_signup.username.data
         email = user_signup.email.data
         password = user_signup.password.data
         confirm_password = user_signup.confirm_password.data
@@ -54,4 +86,4 @@ def signup():
         return f"name: {name}, email: {email}, password: {password}, confirm_password: {confirm_password}"
     else:
         print("not in")
-        return render_template("/signup.html", signup=user_signup)
+    return render_template("/signup.html", signup=user_signup)
